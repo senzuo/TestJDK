70fd809008b56a0b077a59e4f9a4a8394a636f98
senzuo <senzuo@hotmail.com>
2017-08-14 10:55:48 +0800

    typeinfo

--- src/typeinfo/GenericClassReferences.java
+++ src/typeinfo/GenericClassReferences.java
@@ -1,11 +1,14 @@
 package typeinfo;//: typeinfo/GenericClassReferences.java
 
+/**
+ * 泛型引用
+ */
 public class GenericClassReferences {
-  public static void main(String[] args) {
-    Class intClass = int.class;
-    Class<Integer> genericIntClass = int.class;
-    genericIntClass = Integer.class; // Same thing
-    intClass = double.class;
-    // genericIntClass = double.class; // Illegal
-  }
+    public static void main(String[] args) {
+        Class intClass = int.class;
+        Class<Integer> genericIntClass = int.class;
+        genericIntClass = Integer.class; // Same thing
+        intClass = double.class;
+        // genericIntClass = double.class; // Illegal
+    }
 } ///:~
--- src/typeinfo/WildcardClassReferences.java
+++ src/typeinfo/WildcardClassReferences.java
@@ -1,8 +1,11 @@
 package typeinfo;//: typeinfo/WildcardClassReferences.java
 
+/**
+ * 通配符
+ */
 public class WildcardClassReferences {
-  public static void main(String[] args) {
-    Class<?> intClass = int.class;
-    intClass = double.class;
-  }
+    public static void main(String[] args) {
+        Class<?> intClass = int.class;
+        intClass = double.class;
+    }
 } ///:~
